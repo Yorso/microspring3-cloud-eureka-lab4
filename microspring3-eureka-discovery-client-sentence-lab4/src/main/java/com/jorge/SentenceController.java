b3c85d457bfee62ab87c5a7240dc8810509765bc
Yorso <jorgeguzon@gmail.com>
2017-01-16 09:00:54 +0100

    Service names changed

--- microspring3-eureka-discovery-client-sentence-lab4/src/main/java/com/jorge/SentenceController.java
+++ microspring3-eureka-discovery-client-sentence-lab4/src/main/java/com/jorge/SentenceController.java
@@ -20,11 +20,11 @@ public class SentenceController {
 	@RequestMapping("/sentence") 
 	public @ResponseBody String getSentence() {
 		return 
-			getWord("LAB-4-SUBJECT") + " "
-			+ getWord("LAB-4-VERB") + " "
-			+ getWord("LAB-4-ARTICLE") + " "
-			+ getWord("LAB-4-ADJECTIVE") + " "
-			+ getWord("LAB-4-NOUN") + ".";
+			getWord("microspring3-eureka-discovery-client-subject-lab4") + " "
+			+ getWord("microspring3-eureka-discovery-client-verb-lab4") + " "
+			+ getWord("microspring3-eureka-discovery-client-article-lab4") + " "
+			+ getWord("microspring3-eureka-discovery-client-adjective-lab4") + " "
+			+ getWord("microspring3-eureka-discovery-client-noun-lab4") + ".";
 	}
 
 	public String getWord(String service) {

d89c77c2617edb299fce7c974a03e64b6604198d
Yorso <jorgeguzon@gmail.com>
2017-01-16 15:29:52 +0100

    Comments added

--- microspring3-eureka-discovery-client-subject-lab4/src/main/java/com/jorge/WordController.java
+++ microspring3-eureka-discovery-client-subject-lab4/src/main/java/com/jorge/WordController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
 
-	@Value("${words}")
+	@Value("${words}")//These words are got from application.yml (or application.properties)
 	String words;
 
-	@RequestMapping("/getSubject")
+	@RequestMapping("/getSubject") //We are calling this method from SentenceController.java, getSentence() method
 	public @ResponseBody String getWord() {
 		String[] wordArray = words.split(",");
 		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
 		return wordArray[i];
+	}
+	
+
+	@RequestMapping("/")
+	public @ResponseBody String getWordFail() {
+		return "Sentence Client will never get this method"; //Check SentenceController.java, getSentence() method to know why
 	}
 }
